e46e55f4704ce38ea76e0125b4cc2394196ba5f3
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-10-29 14:32:09 -0300

    feat: initial structure

---
+++ src/pages/App.js
@@ -0,0 +1,12 @@
+import githubLogo from '../assets/github-mark-white.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={githubLogo} width={72} height={72} alt="Github logo"/>
+      <h1>GitWiki</h1>
+    </div>
+  );
+}
+
+export default App;
