bb1d0f79dd9d54cbbc81340eda27bfc32037fb30
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-10-29 22:43:21 -0300

    feat: implements remove button

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,14 +1,18 @@
 import React from 'react'
 import { ItemContainer } from "./styles";
 
-export default function Item({ repository }) {
+export default function Item({ repository, handleRemoveRepo }) {
+  const handleRemove = (e) => {
+    e.preventDefault();
+    handleRemoveRepo(repository.id);
+  }
 
   return (
     <ItemContainer>
       <h3>{repository.full_name}</h3>
       <p>{repository.description}</p>
       <a href={repository.html_url} target="_blank" rel="noopener noreferrer" className="repo-details">Details</a>
-      <a href="#" className="repo-remove">Remove</a>
+      <a href="#" onClick={handleRemove} className="repo-remove">Remove</a>
     </ItemContainer>
   )
 }
