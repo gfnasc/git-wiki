af6e329a062fee22ae5822e13be52e541a314027
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2025-10-29 22:40:21 -0300

    feat: implements Item component

--- src/components/Item/index.js
+++ src/components/Item/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { ItemContainer } from "./styles";
 
-export default function Item() {
+export default function Item({ repository }) {
+
   return (
     <ItemContainer>
-      <h3>repo-re</h3>
-      <p>Teste de descrição</p>
-      <a href="#" className="repo-details">Details</a>
+      <h3>{repository.full_name}</h3>
+      <p>{repository.description}</p>
+      <a href={repository.html_url} target="_blank" rel="noopener noreferrer" className="repo-details">Details</a>
       <a href="#" className="repo-remove">Remove</a>
     </ItemContainer>
   )
